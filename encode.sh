bc70c86b529a48bb6d94efe02d6d01fa7d83439e
Tomas Kislan <kislan.tomas@gmail.com>
2019-06-13 12:53:19 +0200

    Encode into subdirectory

--- encode.sh
+++ encode.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-for f in $(ls .downloads); do
+mkdir -p ".downloads/h264"
+
+for f in $(ls .downloads/*.mov); do
     in_file=".downloads/${f}"
-    out_file=".downloads/${f%.*}.mp4"
+    out_file=".downloads/h264/${f%.*}.mp4"
 
     if [ ! -f "${out_file}" ]; then
         HandBrakeCLI --preset-import-file ./handbrake-preset-H264.json -Z H264 -i "${in_file}" -o "${out_file}"
